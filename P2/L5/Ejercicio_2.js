aaa7c15ed6f8ca522e356f67e18ddcc819a0e1f4
Noelia Amengual <n.amengual.2019@alumnos.urjc.es>
2022-03-04 19:52:46 +0100

    Ejercicio 2 L5

---
+++ P2/L5/Ejercicio_2.js
@@ -0,0 +1,11 @@
+console.log("Ejecutando JavaScript...");
+
+const test = document.getElementById('test')
+test.onclick = () => {
+    console.log("Click sobre el párrafo...")
+}
+
+Boton1 = document.getElementById("Boton1")
+Boton1.onclick = () => {
+    console.log("¡Click!")
+}
