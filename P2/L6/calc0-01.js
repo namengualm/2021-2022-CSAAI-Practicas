ba0bdc74f36f013d39050afcfbb015f1aa66b8d7
Noelia Amengual <n.amengual.2019@alumnos.urjc.es>
2022-03-04 20:41:53 +0100

    Ejercicio 1 L6

---
+++ P2/L6/calc0-01.js
@@ -0,0 +1,44 @@
+console.log("Ejecutando JavaScript...");
+
+/* Elementos de la interfaz de la calculadora */
+display = document.getElementById("display")
+boton1 = document.getElementById("boton1")
+boton2 = document.getElementById("boton2")
+suma = document.getElementById("suma")
+resta = document.getElementById("resta")
+igual = document.getElementById("igual")
+clear = document.getElementById("clear")
+
+/* Funciones de retrollamada de los botones... */
+/* Cada vez que se pulse un boton se actua sobre la cadena, */
+/* añadiendo digito 1 + operador + digito 2 + evaluacion de la operacion. */
+
+/* Dígito 1 */
+boton1.onclick = () => {
+    display.innerHTML += "1";
+}
+
+/* Dígito 2 */
+boton2.onclick = () => {
+    display.innerHTML += "2";
+}
+
+/* Insertar simbolo suma */
+suma.onclick = () => {
+    display.innerHTML += "+";
+}
+
+/* Insertar simbolo resta */
+resta.onclick = () => {
+    display.innerHTML += "-";
+}
+
+/* Evaluar la expresión */
+igual.onclick = () => {
+    display.innerHTML = eval(display.innerHTML);
+}
+
+/* Poner a cero la expresión */
+clear.onclick = () => {
+    display.innerHTML = "0";
+}
